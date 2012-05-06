b95a9c4b181c453f4d7c745f93868165e1997cf6
Root <root@localhost>
2012-05-06 17:29:27 +0000

    Converts poc into a proper class file

--- classes/Spawnd.php
+++ classes/Spawnd.php
@@ -1,108 +1,146 @@
 #!/usr/bin/php
 <?php
-$input = (object) getopt('f:');
-$procs = array();
-
-if( !empty( $input->f ) )
+/**
+ * This class is the Spanwd process manager.
+ * @author Thomas Parrott
+ * @package spawnd
+ */
+class Spawnd
 {
-    $procDetail = new StdClass;
-    $procDetail->cmd = $input->f;
-    $procs[] = $procDetail;
-}
+    /**
+     * @var This variable stores information about the managed processes.
+     */
+    private $_procs;
 
-function startProcesses( array &$procs )
-{
-    $descriptorSpec = array(
-        1 => array( 'pipe', 'w' ),  // stdout
-    );
-
-    foreach( $procs as $i => $procDetail )
+    /**
+     * This method initialises the internal processes list array.
+     * @return NULL
+     */
+    public function __construct()
     {
-        updateProcStatus( $procDetail );
+        $this->_procs = array();
+    }
 
-        if( isset( $procDetail->pid ) && !$procDetail->running )
+    /**
+     * This method starts the process manager and enters main loop.
+     * @return NULL
+     */
+    public function run()
+    {
+        while( TRUE )
         {
-            echo "Process " . $procDetail->pid
-                . " has stopped with exit code " . $procDetail->exitcode . "\n";
-        }
-
-        if( !isset( $procDetail->running ) || !$procDetail->running )
-        {
-            $proc = proc_open( $procDetail->cmd, $descriptorSpec, $pipes );
-
-            if( is_resource( $proc ) )
-            {
-                $procDetail->proc   = $proc;
-                $procDetail->stdout = $pipes[ 1 ];
-                stream_set_blocking ( $procDetail->stdout , FALSE );
-                $procs[ $i ] = $procDetail;
-                updateProcStatus( $procDetail );
-                echo "Started process "
-                    . $procDetail->pid . " running " . $procDetail->cmd . "\n";
-                sleep(1);
-            }
+            $this->_startProcesses( $procs );
+            $this->_readProcesses( $procs );
         }
     }
-}
 
-function updateProcStatus( StdClass $procDetail )
-{
-    if( isset( $procDetail->proc ) )
+    /**
+     * This method checks each managed process, and it is not running
+     * then it attempts to start it.
+     * @return NULL
+     */
+    private function _startProcesses()
     {
-        if( $status = proc_get_status( $procDetail->proc ) )
+        $descriptorSpec = array(
+            1 => array( 'pipe', 'w' ),  // stdout
+        );
+
+        foreach( $this->_procs as $i => $procDetail )
         {
-            foreach( $status as $key => $value )
+            updateProcStatus( $procDetail );
+
+            if( isset( $procDetail->pid ) && !$procDetail->running )
             {
-                if( $key === 'exitcode' && $value === -1 )
+                echo "Process " . $procDetail->pid
+                    . " has stopped with exit code "
+                    . $procDetail->exitcode . "\n";
+            }
+
+            if( !isset( $procDetail->running ) || !$procDetail->running )
+            {
+                $proc = proc_open( $procDetail->cmd, $descriptorSpec, $pipes );
+
+                if( is_resource( $proc ) )
                 {
-                    continue;
+                    $procDetail->proc   = $proc;
+                    $procDetail->stdout = $pipes[ 1 ];
+                    stream_set_blocking ( $procDetail->stdout , FALSE );
+                    $procs[ $i ] = $procDetail;
+                    updateProcStatus( $procDetail );
+                    echo "Started process " . $procDetail->pid
+                        . " running " . $procDetail->cmd . "\n";
+                    sleep(1);
                 }
-
-                $procDetail->$key = $value;
             }
         }
     }
-}
 
-
-function streamSelect( array $procs )
-{
-    $readStreams = array();
-
-    //Build a read array.
-    foreach( $procs as $i => $procDetail )
+    /**
+     * This method updates the status information of a managed process.
+     * @param StdClass $procDetail The process object.
+     * @return NULL
+     */
+    private function _updateProcStatus( StdClass $procDetail )
     {
-        $readStreams[ $i ] = $procDetail->stdout;
-    }
-
-    $NULL = NULL;
-
-    if( $readStreams )
-    {
-        $num = stream_select( $readStreams, $NULL, $NULL, 1 );
-        if( $num > 0 )
+        if( isset( $procDetail->proc ) )
         {
-            return $readStreams;
+            if( $status = proc_get_status( $procDetail->proc ) )
+            {
+                foreach( $status as $key => $value )
+                {
+                    if( $key === 'exitcode' && $value === -1 )
+                    {
+                        continue;
+                    }
+
+                    $procDetail->$key = $value;
+                }
+            }
         }
     }
-}
 
-function readProcesses( array &$procs )
-{
-    //Read output from processes.
-    if( $streams = streamSelect( $procs ) )
+
+    /**
+     * This method checks all the processes to see if there is any unread
+     * output on the stdout stream.
+     * @return array stream handles that can be read.
+     */
+    private function _streamSelect()
     {
-        foreach( $streams as $i => $stream )
+        $readStreams = array();
+
+        //Build a read array.
+        foreach( $this->_procs as $i => $procDetail )
         {
-            $buf = fread( $stream, 4096 );
-            echo $buf;
+            $readStreams[ $i ] = $procDetail->stdout;
+        }
+
+        $NULL = NULL;
+
+        if( $readStreams )
+        {
+            $num = stream_select( $readStreams, $NULL, $NULL, 1 );
+            if( $num > 0 )
+            {
+                return $readStreams;
+            }
         }
     }
-}
 
-
-while(1)
-{
-    startProcesses( $procs );
-    readProcesses( $procs );
+    /**
+     * This method reads from any process streams that are ready to be read.
+     * @return NULL
+     */
+    private function _readProcesses()
+    {
+        //Read output from processes.
+        if( $streams = $this->_streamSelect() )
+        {
+            foreach( $streams as $i => $stream )
+            {
+                $buf = fread( $stream, 4096 );
+                echo $buf;
+            }
+        }
+    }
 }
